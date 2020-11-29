5e7498abac0ceb69f1bce5b6e7373c9ab13e213e
s7thiago <thyagosousasilva@gmail.com>
2020-11-29 11:55:18 -0300

    Adding preview from info page body

--- lib/views/about_section.dart
+++ lib/views/about_section.dart
@@ -1,18 +1,35 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class AboutSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Center(
-        child: SingleChildScrollView(
-          child: Stack(
-            children: [
-              Image.asset('assets/documents/resume.png'),
-            ],
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Center(
+          child: SingleChildScrollView(
+            child: Stack(
+              children: [
+                CachedNetworkImage(
+                  imageUrl:
+                      'https://raw.githubusercontent.com/s7Thiago/home_githubio/master/assets/documents/resume.png',
+                  progressIndicatorBuilder: (context, url, progress) {
+                    return Center(
+                      child: LinearProgressIndicator(
+                        value: progress.progress,
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
         ),
+      ),
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
       ),
     );
   }
