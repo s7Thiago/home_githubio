49652bf6fb21c2d2f872436d636fd2ef81932438
Thiago Silva <thyagosousasilva@gmail.com>
2021-09-26 10:35:35 -0300

    Deployed a new web release

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -57,8 +57,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "6ba1f9b5dcedbcac51763be5901394bd",
-"/": "6ba1f9b5dcedbcac51763be5901394bd",
+"index.html": "1c7fa3ef843875a4494dccae4ad89d85",
+"/": "1c7fa3ef843875a4494dccae4ad89d85",
 "main.dart.js": "69bd6144f91a360128da46b441733e57",
 "manifest.json": "b490d2174b5a5707c700307d41390f1e",
 "version.json": "3338f154daa9f0f9d89d0f23981ff19c"
